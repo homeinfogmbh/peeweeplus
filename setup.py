d28a1c54bf0b31c60d9fbcacabb1cc212cd38dbc
Richard Neumann <r.neumann@homeinfo.de>
2017-04-06 12:16:13 +0200

    Renamed package

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 #! /usr/bin/env python3
 
 from distutils.core import setup
-from homeinfo.lib.misc import GitInfo
 
-version, author, author_email, *_ = GitInfo()
 
 setup(
-    name='homeinfo-peewee',
-    version=version,
-    author=author,
+    name='peeweeplus',
+    version='latest',
+    author='Richard Neumann',
     author_email=author_email,
-    requires=['peewee',
-              'homeinfo'],
-    package_dir={'homeinfo': ''},
-    py_modules=['homeinfo.peewee'],
-    description='peewee extensions for HOMEINFO')
+    py_modules=['peeweeplus'],
+    description='Practical peewee extensions')
