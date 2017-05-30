50c32395248b3d5c7800a42d3c3f472dc521aca0
Richard Neumann <r.neumann@homeinfo.de>
2017-05-30 17:56:24 +0200

    Added conversion support for more field types

--- peeweeplus.py
+++ peeweeplus.py
@@ -1,7 +1,9 @@
 """peewee extensions for HOMEINFO"""
 
+from base64 import b64encode
 from contextlib import suppress
 from json import dumps
+from sys import stderr
 from timelib import strpdatetime
 
 import peewee
@@ -92,44 +94,22 @@ class JSONModel(peewee.Model):
     """A JSON-serializable model"""
 
     SHOW_PROTECTED = False
+    DISPLAY_PK = False
     RECURSE_FK = False
     JSON_INDENT = None
 
     def __iter__(self):
         """Yields fields and values formatted for JSON complinace"""
         for attr in dir(self.__class__):
-            if not attr.startswith('_') or self.__class__.SHOW_PROTECTED:
-                cls_val = getattr(self.__class__, attr)
+            if not attr.startswith('_') or self.SHOW_PROTECTED:
+                field = getattr(self.__class__, attr)
 
-                if isinstance(cls_val, peewee.Field):
-                    if isinstance(cls_val, peewee.IntegerField):
-                        val = getattr(self, attr)
-                    elif isinstance(cls_val, peewee.DecimalField):
-                        val = getattr(self, attr)
-
-                        if val is not None:
-                            val = float(val)
-                    elif isinstance(cls_val, peewee.FloatField):
-                        val = getattr(self, attr)
-                    elif isinstance(cls_val, peewee.CharField):
-                        val = getattr(self, attr)
-                    elif isinstance(cls_val, peewee.TextField):
-                        val = getattr(self, attr)
-                    elif isinstance(cls_val, peewee.DateTimeField):
-                        val = getattr(self, attr)
-
-                        if val is not None:
-                            val = val.isoformat()
-                    elif isinstance(cls_val, peewee.DateField):
-                        val = getattr(self, attr)
-
-                        if val is not None:
-                            val = val.isoformat()
-                    elif isinstance(cls_val, peewee.ForeignKeyField):
+                if isinstance(field, peewee.Field):
+                    if isinstance(field, peewee.ForeignKeyField):
                         model = getattr(self, attr)
 
                         if model is not None:
-                            if self.__class__.RECURSE_FK:
+                            if self.RECURSE_FK:
                                 try:
                                     val = dict(model)
                                 except (TypeError, ValueError):
@@ -138,11 +118,52 @@ class JSONModel(peewee.Model):
                                 val = model._get_pk_value()
                         else:
                             val = model
+                    elif isinstance(field, peewee.IntegerField):
+                        if isinstance(field, peewee.PrimaryKeyField):
+                            if not self.DISPLAY_PK:
+                                continue
+
+                        val = getattr(self, attr)
+                    elif isinstance(field, peewee.DecimalField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = float(val)
+                    elif isinstance(field, peewee.FloatField):
+                        val = getattr(self, attr)
+                    elif isinstance(field, peewee.CharField):
+                        val = getattr(self, attr)
+                    elif isinstance(field, peewee.TextField):
+                        val = getattr(self, attr)
+                    elif isinstance(field, peewee.DateTimeField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = val.isoformat()
+                    elif isinstance(field, peewee.DateField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = val.isoformat()
+                    elif isinstance(field, peewee.TimeField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = val.isoformat()
+                    elif isinstance(field, peewee.BooleanField):
+                        val = getattr(self, attr)
+                    elif isinstance(field, peewee.BlobField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = b64encode(val)
                     else:
+                        print('Unknown Field type: {}'.format(field),
+                              file=stderr)
                         continue
 
                     yield (attr, val)
 
     def __str__(self):
         """Returns the model as a JSON string"""
-        return dumps(dict(self), indent=self.__class__.JSON_INDENT)
+        return dumps(dict(self), indent=self.JSON_INDENT)
