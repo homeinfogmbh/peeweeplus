3089e0457205a4b3b9d6a1216ab5f9ac5e7725c1
Richard Neumann <r.neumann@homeinfo.de>
2017-05-30 17:36:39 +0200

    Added JSONModel()

--- peeweeplus.py
+++ peeweeplus.py
@@ -12,7 +12,8 @@ __all__ = [
     'dec2orm',
     'date2orm',
     'datetime2orm',
-    'MySQLDatabase']
+    'MySQLDatabase',
+    'JSONModel']
 
 
 def create(model):
@@ -84,3 +85,58 @@ class MySQLDatabase(peewee.MySQLDatabase):
                 return super().execute_sql(*args, **kwargs)
         else:
             return super().execute_sql(*args, **kwargs)
+
+
+class JSONModel(peewee.Model):
+    """A JSON-serializable model"""
+
+    SHOW_PROTECTED = False
+    RECURSE_FK = False
+
+    def __iter__(self):
+        """Yields fields and values formatted for JSON complinace"""
+        for attr in dir(self.__class__):
+            if not attr.startswith('_') or self.__class__.SHOW_PROTECTED:
+                cls_val = getattr(self.__class__, attr)
+
+                if isinstance(cls_val, peewee.Field):
+                    if isinstance(cls_val, peewee.IntegerField):
+                        val = getattr(self, attr)
+                    elif isinstance(cls_val, peewee.DecimalField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = float(val)
+                    elif isinstance(cls_val, peewee.FloatField):
+                        val = getattr(self, attr)
+                    elif isinstance(cls_val, peewee.CharField):
+                        val = getattr(self, attr)
+                    elif isinstance(cls_val, peewee.TextField):
+                        val = getattr(self, attr)
+                    elif isinstance(cls_val, peewee.DateTimeField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = val.isoformat()
+                    elif isinstance(cls_val, peewee.DateField):
+                        val = getattr(self, attr)
+
+                        if val is not None:
+                            val = val.isoformat()
+                    elif isinstance(cls_val, peewee.ForeignKeyField):
+                        model = getattr(self, attr)
+
+                        if model is not None:
+                            if self.__class__.RECURSE_FK:
+                                try:
+                                    val = dict(model)
+                                except (TypeError, ValueError):
+                                    val = '<Not JSON serializable>'
+                            else:
+                                val = model._get_pk_value()
+                        else:
+                            val = model
+                    else:
+                        continue
+
+                    yield (attr, val)
