199055468e48a6f5889fc0c28706adea33e3a9b9
Richard Neumann <r.neumann@homeinfo.de>
2018-01-24 15:33:00 +0100

    Added monkey patches.

--- peeweeplus/__init__.py
+++ peeweeplus/__init__.py
@@ -6,11 +6,13 @@ from peeweeplus.database import MySQLDatabase
 from peeweeplus.fields import InvalidEnumerationValue, EnumField
 from peeweeplus.json import FieldValueError, FieldNotNullable, iterfields, \
     deserialize, serialize, JSONModel
+from peeweeplus.monkeypatches import monkeypatch
 
 __all__ = [
     'FieldValueError',
     'FieldNotNullable',
     'InvalidEnumerationValue',
+    'monkeypatch',
     'dec2dom',
     'dec2dict',
     'dec2orm',
---
+++ peeweeplus/monkeypatches.py
@@ -0,0 +1,23 @@
+"""Monkey patches for peewee."""
+
+from peewee import BaseModel
+
+__all__ = ['monkeypatch']
+
+
+def by_primary_key(model, pk_value):
+    """Gets a record by its primary key."""
+
+    return model.get(model._meta.primary_key == pk_value)
+
+
+def monkeypatch(getitem=True):
+    """Applies the respective monkey patches."""
+
+    if getitem:
+        # Enables:
+        #     try:
+        #         record_with_pk_12 = Model[12]
+        #     except DoesNotExist:
+        #         print('No record with primary key 12.')
+        BaseModel.__getitem__ = by_primary_key
