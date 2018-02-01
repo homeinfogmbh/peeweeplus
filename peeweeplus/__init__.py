76e0313f634e8caeb49a9e37ae9ec74d60fd17dd
Richard Neumann <mail@richard-neumann.de>
2018-02-01 23:40:33 +0100

    Monkex patched Field.

--- peeweeplus/__init__.py
+++ peeweeplus/__init__.py
@@ -1,5 +1,7 @@
 """Practical extensions of the peewee ORM framework."""
 
+from peewee import Field
+
 from peeweeplus.converters import dec2dom, dec2dict, dec2orm, date2orm, \
     datetime2orm
 from peeweeplus.database import MySQLDatabase
@@ -21,3 +23,15 @@ __all__ = [
     'MySQLDatabase',
     'JSONModel',
     'EnumField']
+
+
+# Quick fix.
+def _get_db_column(self):
+    return self.column_name
+
+
+def _set_db_column(self, db_column):
+    self.column_name = db_column
+
+
+Field.db_column = property(_get_db_column, _set_db_column)
