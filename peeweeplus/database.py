ca13479d8fb015c1968277e4023515330082cafa
Richard Neumann <r.neumann@homeinfo.de>
2018-05-23 13:09:51 +0200

    Added from_config() classmethod.

--- peeweeplus/database.py
+++ peeweeplus/database.py
@@ -13,6 +13,13 @@ class MySQLDatabase(_MySQLDatabase):
         super().__init__(*args, **kwargs)
         self.closing = closing
 
+    @classmethod
+    def from_config(cls, config, closing=True):
+        """Creates a database from the respective configuration."""
+        return cls(
+            config['db'], host=config['host'], user=config['user'],
+            passwd=config['passwd'], closing=config.get('closing', closing))
+
     def execute_sql(self, *args, **kwargs):
         """Conditionally execute the SQL query in an
         execution context iff closing is enabled.
