732142a0a983d97136313057c9cfd094610163d5
Richard Neumann <r.neumann@homeinfo.de>
2018-08-22 15:11:45 +0200

    Added JSONModel methods json_select() and json_get().

--- peeweeplus/json/model.py
+++ peeweeplus/json/model.py
@@ -2,7 +2,7 @@
 
 from peewee import Model
 
-from peeweeplus.json.deserialization import deserialize
+from peeweeplus.json.deserialization import CONVERTER, deserialize
 from peeweeplus.json.fields import json_fields
 from peeweeplus.json.serialization import serialize
 
@@ -17,3 +17,28 @@ class JSONModel(Model):
     from_json = classmethod(deserialize)
     patch_json = deserialize
     to_json = serialize
+
+    @classmethod
+    def json_select(cls, json, skip=None):
+        """Selects from the model from the respective JSON dict."""
+        skip = skip or frozenset()
+        select = True
+
+        for key, value in json.items():
+            if key in skip:
+                continue
+
+            field = getattr(cls, key)
+            value = CONVERTER(field, value, check_null=False)
+
+            if value is None:
+                select &= field >> None
+            else:
+                select &= field == value
+
+        return cls.select().where(select)
+
+    @classmethod
+    def json_get(cls, json, skip=None):
+        """Gets the first matched record from the respective JSON dict."""
+        return cls.json_select(json, skip=skip).get()
