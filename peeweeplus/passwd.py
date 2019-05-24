09cd15a3fea37e9363c2c7c874777f7d1fca724e
Richard Neumann <r.neumann@homeinfo.de>
2019-05-24 09:11:48 +0200

    Refac.

--- peeweeplus/passwd.py
+++ peeweeplus/passwd.py
@@ -1,7 +1,5 @@
 """Argon2-based password hashing."""
 
-from collections import namedtuple
-
 from argon2 import extract_parameters
 from peewee import FieldAccessor
 
@@ -11,12 +9,17 @@ from peeweeplus.exceptions import PasswordTooShortError
 __all__ = ['Argon2Hash', 'Argon2FieldAccessor']
 
 
-class Argon2Hash(namedtuple('Argon2Hash', ('hash', 'hasher'))):
+class Argon2Hash(str):
     """An Argon2 hash."""
 
-    def __str__(self):
-        """Returns the hash string."""
-        return self.hash
+    def __new__(cls, string, _):
+        """Retuns a new Argon2Hash."""
+        return super().__new__(cls, string)
+
+    def __init__(self, _, hasher):
+        """Sets the hasher."""
+        super().__init__()
+        self.hasher = hasher
 
     @classmethod
     def from_plaintext(cls, plaintext, hasher):
@@ -26,16 +29,16 @@ class Argon2Hash(namedtuple('Argon2Hash', ('hash', 'hasher'))):
     @property
     def needs_rehash(self):
         """Determines whether the password needs a rehash."""
-        return self.hasher.check_needs_rehash(self.hash)
+        return self.hasher.check_needs_rehash(self)
 
     @property
     def parameters(self):
         """Returns the Argon2 hash parameters."""
-        return extract_parameters(self.hash)
+        return extract_parameters(self)
 
     def verify(self, passwd):
         """Validates the plain text password against this hash."""
-        return self.hasher.verify(self.hash, passwd)
+        return self.hasher.verify(self, passwd)
 
 
 class Argon2FieldAccessor(FieldAccessor):   # pylint: disable=R0903
