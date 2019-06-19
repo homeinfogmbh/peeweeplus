cdffe44e13c2e80b43c9b828bd46ff9e99a0f9be
Richard Neumann <r.neumann@homeinfo.de>
2019-06-19 16:14:57 +0200

    Refac.

--- peeweeplus/json/serialization.py
+++ peeweeplus/json/serialization.py
@@ -1,6 +1,7 @@
 """JSON serialization."""
 
 from base64 import b64encode
+from contextlib import suppress
 
 from peewee import BlobField
 from peewee import DateField
@@ -28,7 +29,29 @@ CONVERTER = FieldConverter(
     (IPv4AddressField, str))
 
 
-def serialize(record, *, null=False, cascade=False, **filters):
+def _check_cascade(key, attribute, cascade):
+    """Returns foreign key cascading information."""
+
+    if not cascade:
+        return (False, None)
+
+    if isinstance(cascade, int):
+        return (True, cascade - 1) if cascade > 0 else (True, cascade + 1)
+
+    if isinstance(cascade, bool):
+        return (True, cascade)
+
+    if isinstance(cascade, dict):
+        with suppress(KeyError):
+            return (True, cascade[key])
+
+        with suppress(KeyError):
+            return (True, cascade[attribute])
+
+    return (False, None)
+
+
+def serialize(record, *, null=False, cascade=None, **filters):
     """Returns a JSON-ish dict with the record's fields' values."""
 
     model = type(record)
@@ -39,18 +62,12 @@ def serialize(record, *, null=False, cascade=False, **filters):
     for key, attribute, field in fields_filter.filter(fields):
         value = getattr(record, attribute)
         value = CONVERTER(field, value, check_null=False)
+        do_cascade, cascade_next = _check_cascade(key, attribute, cascade)
 
-        if cascade and isinstance(field, ForeignKeyField):
-            next_cascade = True if cascade is True else cascade - 1
-            rel_model = field.rel_model
-
-            try:
-                rel_record = rel_model[value]
-            except rel_model.DoesNotExist:
-                pass
-            else:
-                value = rel_record.to_json(
-                    null=null, cascade=next_cascade, **filters)
+        if do_cascade and isinstance(field, ForeignKeyField):
+            with suppress(field.rel_model.DoesNotExist, AttributeError):
+                value = field.rel_model[value].to_json(
+                    null=null, cascade=cascade_next, **filters)
 
         if not null and value is None:
             continue
