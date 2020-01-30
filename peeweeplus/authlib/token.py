4d33d14d7303cb76306550721a2157f33a063867
Richard Neumann <r.neumann@homeinfo.de>
2020-01-30 15:41:32 +0100

    Implemented OAuth2AuthorizationCodeMixin.

--- peeweeplus/authlib/token.py
+++ peeweeplus/authlib/token.py
@@ -7,12 +7,12 @@ from peewee import BooleanField, CharField, IntegerField, TextField
 from peeweeplus.exceptions import MissingModule
 
 try:
-    from authlib.oauth2.rfc6749 import TokenMixin
+    from authlib.oauth2.rfc6749 import TokenMixin, AuthorizationCodeMixin
 except ModuleNotFoundError:
     raise MissingModule('authlib')
 
 
-__all__ = ['OAuth2TokenMixin']
+__all__ = ['OAuth2TokenMixin', 'OAuth2AuthorizationCodeMixin']
 
 
 class OAuth2TokenMixin(TokenMixin):
@@ -42,3 +42,37 @@ class OAuth2TokenMixin(TokenMixin):
     def get_expires_at(self):
         """Returns the timstamp in microseconds when the token expires."""
         return self.issued_at + self.expires_in
+
+
+class OAuth2AuthorizationCodeMixin(AuthorizationCodeMixin):
+    """Mixin for OAuth 2.0 authorization codes."""
+
+    code = CharField(120, unique=True)
+    client_id = CharField(48, null=True)
+    redirect_uri = TextField(default='')
+    response_type = TextField(default='')
+    scope = TextField(default='')
+    nonce = TextField(null=True)
+    auth_time = IntegerField(default=lambda: int(time()))
+    code_challenge = TextField(null=True)
+    code_challenge_method = CharField(48, null=True)
+
+    def is_expired(self):
+        """Determines whether the autorization code is expired."""
+        return self.auth_time + 300 < time()
+
+    def get_redirect_uri(self):
+        """Returns a redirect URI."""
+        return self.redirect_uri
+
+    def get_scope(self):
+        """Returns the scope."""
+        return self.scope
+
+    def get_auth_time(self):
+        """Returns the authentication time."""
+        return self.auth_time
+
+    def get_nonce(self):
+        """Returns the nonce."""
+        return self.nonce
