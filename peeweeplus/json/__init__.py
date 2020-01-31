5a2ddc9ad75570c691ce3c82a28e6d3f08b45cbf
Richard Neumann <r.neumann@homeinfo.de>
2020-01-31 11:06:38 +0100

    Added patch().

--- peeweeplus/json/__init__.py
+++ peeweeplus/json/__init__.py
@@ -1,8 +1,8 @@
 """JSON serialization and deserialization API."""
 
-from peeweeplus.json.deserialization import deserialize
+from peeweeplus.json.deserialization import deserialize, patch
 from peeweeplus.json.model import JSONMixin, JSONModel
 from peeweeplus.json.serialization import serialize
 
 
-__all__ = ['deserialize', 'serialize', 'JSONMixin', 'JSONModel']
+__all__ = ['deserialize', 'patch', 'serialize', 'JSONMixin', 'JSONModel']
