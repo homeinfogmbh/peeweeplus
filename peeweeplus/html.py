193d8b867b1df4a8ac7f44fa0074712026806399
Richard Neumann <r.neumann@homeinfo.de>
2020-11-27 13:14:31 +0100

    Added <p> stripping.

--- peeweeplus/html.py
+++ peeweeplus/html.py
@@ -1,6 +1,7 @@
 """Sanitizing HTML."""
 
 from functools import lru_cache
+from re import compile  # pylint: disable=W0622
 
 from lxml.etree import XMLSyntaxError   # pylint: disable=E0611
 from lxml.html.clean import Cleaner     # pylint: disable=E0611
@@ -10,14 +11,12 @@ __all__ = ['ALLOWED_TAGS', 'CLEANER', 'sanitize']
 
 
 ALLOWED_TAGS = {
-    'br', 'div', 'em', 'font', 'li', 'ol', 'p', 'span', 'strong', 'table',
-    'tbody', 'td', 'th', 'thead', 'tr', 'ul'
+    'a', 'br', 'div', 'em', 'font', 'i', 'li', 'ol', 'p', 'span', 'strong',
+    'table', 'tbody', 'td', 'th', 'thead', 'tr', 'u', 'ul'
 }
-CLEANER = Cleaner(
-    allow_tags=ALLOWED_TAGS,
-    remove_unknown_tags=False,
-    page_structure=True
-)
+CLEANER = Cleaner(allow_tags=ALLOWED_TAGS, remove_unknown_tags=False)
+P_TAG = '<p>'
+P_REGEX = compile('<p>(.*)</p>')
 
 
 @lru_cache()
@@ -25,6 +24,14 @@ def sanitize(text: str, *, cleaner: Cleaner = CLEANER) -> str:
     """Sanitizes the respective HTML text."""
 
     try:
-        return cleaner.clean_html(text)
+        cleaned_text = cleaner.clean_html(text)
     except XMLSyntaxError:  # Probably not HTML text.
         return text
+
+    if cleaned_text.startswith(P_TAG) and not text.startswith(P_TAG):
+        match = P_REGEX.fullmatch(cleaned_text)
+
+        if match is not None:
+            return match.group(1)
+
+    return cleaned_text
