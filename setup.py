8cabd99d66fb799556eebe7fe539ecbb72a02809
Richard Neumann <r.neumann@homeinfo.de>
2020-11-27 12:35:35 +0100

    Migrated to setuptools-git-version.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='peeweeplus',
-    version='latest',
+    version_format='{tag}',
+    setup_requires=['setuptools-git-version'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
     maintainer_email='<r dot neumann at homeinfo priod de>',
     packages=['peeweeplus', 'peeweeplus.fields', 'peeweeplus.json'],
     license='GPLv3',
-    description='Practical extensions for the peewee ORM framework.')
+    description='Practical extensions for the peewee ORM framework.'
+)
