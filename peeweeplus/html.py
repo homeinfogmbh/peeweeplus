82d41770e11e15ad3503e5d595f5bd58ef8ac6eb
Richard Neumann <r.neumann@homeinfo.de>
2020-12-03 12:50:37 +0100

    Added stripping of <p> tag created by Cleaner.clean_html().

--- peeweeplus/html.py
+++ peeweeplus/html.py
@@ -1,9 +1,10 @@
 """Sanitizing HTML."""
 
 from functools import lru_cache
+from typing import Generator
 
 from lxml.etree import XMLSyntaxError   # pylint: disable=E0611
-from lxml.html import document_fromstring, tostring
+from lxml.html import Element, document_fromstring, tostring
 from lxml.html.clean import Cleaner     # pylint: disable=E0611
 
 
@@ -17,9 +18,22 @@ ALLOWED_TAGS = {
 CLEANER = Cleaner(allow_tags=ALLOWED_TAGS, remove_unknown_tags=False)
 
 
+def get_html(element: Element) -> Generator[bytes, None, None]:
+    """Returns HTML text from an element."""
+
+    children = element.getchildren()
+
+    # Remove <p>…</p> wrapper created by Cleaner.clean_html().
+    if len(children) == 1 and children[0].tag == 'p':
+        for child in children[0].iterchildren():
+            yield tostring(child)
+    else:
+        for child in children:
+            yield tostring(child)
+
+
 @lru_cache(maxsize=None)
-def sanitize(text: str, *, cleaner: Cleaner = CLEANER,
-             encoding: str = 'utf-8') -> str:
+def sanitize(text: str, *, cleaner: Cleaner = CLEANER) -> str:
     """Sanitizes the respective HTML text."""
 
     try:
@@ -27,7 +41,4 @@ def sanitize(text: str, *, cleaner: Cleaner = CLEANER,
     except XMLSyntaxError:  # Probably not HTML text.
         return text
 
-    return b''.join(
-        tostring(elem, encoding=encoding)
-        for elem in cleaner.clean_html(doc).iterchildren()
-    ).decode(encoding)
+    return b''.join(get_html(cleaner.clean_html(doc))).decode()
