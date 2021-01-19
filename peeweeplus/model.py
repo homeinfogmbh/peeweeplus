47c8ce9587023050418700838b9875f8b027dd1d
Richard Neumann <mail@richard-neumann.de>
2021-01-19 19:22:48 +0100

    Debugging.

--- peeweeplus/model.py
+++ peeweeplus/model.py
@@ -44,9 +44,9 @@ def join_tree(model: ModelBase) -> Iterator[JoinCondition]:
     for attribute, field in get_foreign_keys(model):
         rel_model = field.rel_model.alias()
         join_type = JOIN.LEFT_OUTER if field.null else JOIN.INNER
-        condition = field == field.rel_field
+        condition = field == rel_model.id
         yield JoinCondition(model, rel_model, join_type, condition, attribute)
-        yield from join_tree(rel_model)
+        yield from join_tree(field.rel_model)
 
 
 def select_tree(model: ModelBase) -> ModelSelect:
