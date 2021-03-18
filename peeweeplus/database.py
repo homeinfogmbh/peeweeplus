3175f96338474bcd7b3b56fa7ef80007cd098d1e
Richard Neumann <mail@richard-neumann.de>
2021-03-18 14:53:22 +0100

    Add deferred config access to database

--- peeweeplus/database.py
+++ peeweeplus/database.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from configparser import SectionProxy
-from typing import Any
+from typing import Any, Optional
 
 from peewee import OperationalError, MySQLDatabase
 
@@ -13,24 +13,63 @@ __all__ = ['MySQLDatabase']
 class MySQLDatabase(MySQLDatabase):     # pylint: disable=E0102,W0223
     """Extension of peewee.MySQLDatabase with closing option."""
 
-    def __init__(self, database: str, closing: bool = False,
-                 retry: bool = False, **kwargs):
-        """Invokes super's init method."""
-        super().__init__(database, **kwargs)
-        self.closing = closing
-        self.retry = retry
+    def init(self, database: str, *, config: Optional[SectionProxy] = None,
+             **kwargs) -> None:
+        """Initializes the database."""
+        super().init(database, **kwargs)
+        self.config = config
+
+    @property
+    def database(self) -> Optional[str]:
+        """Returns the database name."""
+        if self.config is None:
+            return self._database
+
+        return self.config.get('db', fallback=self.config.get('database'))
+
+    @database.setter
+    def databse(self, database: Optional[str]) -> None:
+        """Sets the database name."""
+        self._database = database
+
+    @property
+    def connect_params(self) -> dict:
+        """Returns the connection parameters."""
+        if self.config is None:
+            return self._connect_params
+
+        return {
+            **self._connect_params,
+            'host': self.config.get('host'),
+            'user': self.config.get('user'),
+            'passwd': self.config.get('passwd')
+        }
+
+    @connect_params.setter
+    def connect_params(self, connect_params: dict) -> None:
+        """Sets the connection parameters."""
+        self._connect_params = connect_params
+
+    @property
+    def closing(self) -> bool:
+        """Determines whether the database is auto-closing."""
+        if self.config is None:
+            return False
+
+        return self.config.getboolean('closing', True)
+
+    @property
+    def retry(self) -> bool:
+        """Determines whether the database is auto-retrying."""
+        if self.config is None:
+            return False
+
+        return self.config.getboolean('retry', False)
 
     @classmethod
     def from_config(cls, config: SectionProxy) -> MySQLDatabase:
         """Creates a database from the respective configuration."""
-        database = config.get('db', fallback=config.get('database'))
-        host = config.get('host')
-        user = config.get('user')
-        passwd = config.get('passwd', fallback=config.get('password'))
-        closing = config.getboolean('closing', True)
-        retry = config.getboolean('retry', False)
-        return cls(database, host=host, user=user, passwd=passwd,
-                   closing=closing, retry=retry)
+        return cls(None, config=config)
 
     # pylint: disable=W0221
     def execute_sql(self, *args, retried: bool = False, **kwargs) -> Any:
