bcb3a47fc02d4b068ee7e9ed637a2838bc46c37d
Richard Neumann <mail@richard-neumann.de>
2021-12-04 02:17:01 +0100

    Add dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,15 @@ setup(
     name='peeweeplus',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
+    install_requires=[
+        'configlib',
+        'peewee',
+        'lxml'
+    ],
+    extras_require={
+        'Argon2Field': ['argon2_cffi'],
+        'authlib integration': ['authlib']
+    },
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
