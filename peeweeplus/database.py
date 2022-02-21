963ea80579f8f3704512993e5a1010aacfc20da6
Richard Neumann <r.neumann@homeinfo.de>
2022-02-21 12:44:24 +0100

    Fix double-closing of database.
    Fixes https://github.com/homeinfogmbh/comcat/issues/25

--- peeweeplus/database.py
+++ peeweeplus/database.py
@@ -3,7 +3,7 @@
 from logging import getLogger
 from typing import Any
 
-from peewee import OperationalError, MySQLDatabase
+from peewee import MySQLDatabase as _MySQLDatabase
 
 
 __all__ = ['MySQLDatabase']
@@ -12,13 +12,18 @@ __all__ = ['MySQLDatabase']
 LOGGER = getLogger(__file__)
 
 
-class MySQLDatabase(MySQLDatabase):     # pylint: disable=E0102,W0223
+class MySQLDatabase(_MySQLDatabase):
     """Extension of peewee.MySQLDatabase with closing option."""
 
-    # pylint: disable=W0221
     def execute_sql(self, *args, **kwargs) -> Any:
         """Conditionally execute the SQL query in an
         execution context iff closing is enabled.
         """
+        if self._state.transactions:
+            return super().execute_sql(*args, **kwargs)
+
         with self.connection_context():
             return super().execute_sql(*args, **kwargs)
+
+    def sequence_exists(self, seq):     # Only used on PostgreSQL databases
+        pass
