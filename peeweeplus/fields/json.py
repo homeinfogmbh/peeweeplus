5be2c22b58265771645e07a89b3eddce3b106a9e
Richard Neumann <mail@richard-neumann.de>
2022-02-24 21:07:27 +0100

    Change None de-/serialization

--- peeweeplus/fields/json.py
+++ peeweeplus/fields/json.py
@@ -1,7 +1,7 @@
 """Text-based JSON storage."""
 
 from json import loads, dumps
-from typing import Callable
+from typing import Callable, Optional
 
 from peewee import TextField
 
@@ -14,29 +14,31 @@ __all__ = ['JSONTextField']
 class JSONTextField(TextField):
     """Stores JSON as text."""
 
-    def __init__(self, *args, serialize: Callable = dumps,
-                 deserialize: Callable = loads, **kwargs):
+    def __init__(
+            self,
+            *args,
+            serialize: Callable[[JSON], str] = dumps,
+            deserialize: Callable[[str], JSON] = loads,
+            **kwargs
+    ):
         """Sets the respective encoding and decoding functions."""
         super().__init__(*args, **kwargs)
         self.serialize = serialize
         self.deserialize = deserialize
 
-    def db_value(self, value: JSON) -> str:
+    def db_value(self, value: JSON) -> Optional[str]:
         """Returns a string for the database."""
         if value is None:
             if self.null:
                 return None
 
-            return self.serialize({})
+            return self.serialize(None)
 
         return self.serialize(value)
 
-    def python_value(self, value: str) -> JSON:
+    def python_value(self, value: Optional[str]) -> JSON:
         """Returns a JSON object for python."""
         if value is None:
-            if self.null:
-                return None
-
-            return {}
+            return None
 
         return self.deserialize(value)
