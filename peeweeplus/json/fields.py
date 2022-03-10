b3e52f75e467c57b211a95c2b60023ac788a7780
Richard Neumann <mail@richard-neumann.de>
2022-03-10 02:32:06 +0100

    Refactoring

--- peeweeplus/json/fields.py
+++ peeweeplus/json/fields.py
@@ -2,9 +2,9 @@
 
 from contextlib import suppress
 from functools import lru_cache
-from typing import Iterable, Iterator, NamedTuple, Set
+from typing import Any, Iterable, Iterator, NamedTuple, Type
 
-from peewee import Field, ForeignKeyField, ModelBase
+from peewee import Field, ForeignKeyField, Model
 
 from peeweeplus.exceptions import NullError
 
@@ -24,8 +24,13 @@ class JSONField(NamedTuple):
     field: Field
 
 
-def contains(iterable: Iterable, key: str, attribute: str, *,
-             default: bool = False) -> bool:
+def contains(
+        iterable: Iterable,
+        key: str,
+        attribute: str,
+        *,
+        default: bool = False
+) -> bool:
     """Determines whether the field is contained within the iterable."""
 
     if iterable:
@@ -34,48 +39,24 @@ def contains(iterable: Iterable, key: str, attribute: str, *,
     return default
 
 
-def _get_json_fields(model: ModelBase) -> Iterator[JSONField]:
-    """Yields the JSON fields of the respective model."""
-
-    fields = model._meta.fields     # pylint: disable=W0212
-
-    try:
-        key_formatter = model.__key_formatter__
-    except AttributeError:
-        key_formatter = lambda x: x
-
-    for attribute, field in fields.items():
-        if attribute.startswith('_'):
-            continue
-
-        if isinstance(field, ForeignKeyField):
-            if attribute.endswith('_id') and attribute + '_id' not in fields:
-                continue
-
-        yield JSONField(key_formatter(field.column_name), field.name, field)
-
-
-def get_json_fields(model: ModelBase) -> Set[JSONField]:
-    """Returns the JSON fields of the respective model
-    and caches it in the JSON_FIELDS dict.
-    """
-
-    with suppress(KeyError):
-        return JSON_FIELDS[model]
-
-    JSON_FIELDS[model] = fields = frozenset(_get_json_fields(model))
-    return fields
-
-
 class FieldConverter(dict):
     """Maps conversion functions to field classes."""
 
-    def __call__(self, field: Field, value: object,
-                 check_null: bool = False) -> object:
+    def __call__(
+            self,
+            field: Field,
+            value: Any,
+            check_null: bool = False
+    ) -> Any:
         """Converts the respective value to the field."""
         @lru_cache(maxsize=CACHE_LIMIT, typed=True)
-        def cached(field: Field, value: object, check_null: bool):
+        def _convert_field_value(
+                field: Field,
+                value: Any,
+                check_null: bool
+        ) -> Any:
             """Caches the result."""
+
             if value is None:
                 if check_null and not field.null:
                     raise NullError()
@@ -95,4 +76,42 @@ class FieldConverter(dict):
 
             return value
 
-        return cached(field, value, check_null)
+        return _convert_field_value(field, value, check_null)
+
+
+def _get_json_fields(model: Type[Model]) -> Iterator[JSONField]:
+    """Yields the JSON fields of the respective model."""
+
+    fields = model._meta.fields     # pylint: disable=W0212
+
+    try:
+        key_formatter = model.__key_formatter__
+    except AttributeError:
+        key_formatter = None
+
+    for attribute, field in fields.items():
+        if attribute.startswith('_'):
+            continue
+
+        if isinstance(field, ForeignKeyField):
+            if attribute.endswith('_id') and attribute + '_id' not in fields:
+                continue
+
+        if key_formatter is not None:
+            key = key_formatter(field.column_name)
+        else:
+            key = field.column_name
+
+        yield JSONField(key, field.name, field)
+
+
+def get_json_fields(model: Type[Model]) -> frozenset[JSONField]:
+    """Returns the JSON fields of the respective model
+    and caches it in the JSON_FIELDS dict.
+    """
+
+    with suppress(KeyError):
+        return JSON_FIELDS[model]
+
+    JSON_FIELDS[model] = fields = frozenset(_get_json_fields(model))
+    return fields
