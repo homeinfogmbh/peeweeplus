3bac0e5fcb93bca53d2f626e0a17b34e7cf0905c
Richard Neumann <mail@richard-neumann.de>
2022-03-10 02:51:52 +0100

    Simplify camel_case()

--- peeweeplus/json/functions.py
+++ peeweeplus/json/functions.py
@@ -1,30 +1,12 @@
 """JSON-related functions."""
 
-from typing import Iterator
+from re import sub
 
 
 __all__ = ['camel_case']
 
 
-def _camel_case(string: str, *, first_caps: bool = False) -> Iterator[str]:
-    """Yields characters converted to camelCase."""
-
-    underscore = False
-
-    for index, char in enumerate(string):
-        if char == '_':
-            underscore = True
-        elif underscore:
-            underscore = False
-            yield char.upper()
-        elif index == 0 and first_caps:
-            yield char.upper()
-        else:
-            yield char
-
-
-def camel_case(string: str, *, first_caps: bool = False) -> str:
+def camel_case(string: str) -> str:
     """Converts a snake_case string to camelCase."""
 
-    string = string.strip('_')
-    return ''.join(_camel_case(string, first_caps=first_caps))
+    return sub(r'_(\w)', lambda match: match.group(1).upper(), string)
